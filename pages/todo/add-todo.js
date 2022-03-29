2836e95bd55458f6ea29c9378777d2a667bb2557
intanirnanda5796 <intan.irnanda@gmail.com>
2022-03-29 07:37:45 +0700

    fixing list and submit title

--- pages/todo/add-todo.js
+++ pages/todo/add-todo.js
@@ -1,10 +1,31 @@
-import { Container } from "@mui/material"
+import { useRouter } from "next/router";
+import { Box, Button, Container, TextField } from "@mui/material";
+import { useAtom } from "jotai";
+import { newTodo, addTodoAtom } from "../../store";
 
-export default function AddTodo()
-{
-    return (
-        <Container sx={{ width: '500px', marginTop: '20px' }}>
-            <p>test</p>
-        </Container>
-    )
+export default function AddTodo() {
+  const [data, setData] = useAtom(newTodo);
+  const [, addTodo] = useAtom(addTodoAtom);
+  const route = useRouter();
+
+  console.log(data);
+
+  const handleSubmit = () => {
+      addTodo(data);
+      route.push('/todo');
+  }
+
+  return (
+    <Container sx={{ width: "500px", marginTop: "20px" }}>
+      <Box component="form" autoComplete="off">
+        <TextField
+          label="Title"
+          type="text"
+          value={data.title || ''}
+          onChange={(e) => setData({ title: e.target.value }) }
+        />
+        <Button type="submit" variant="contained" onClick={() => handleSubmit()}>Submit</Button>
+      </Box>
+    </Container>
+  );
 }
--- store/index.js
+++ store/index.js
@@ -56,14 +56,36 @@ const addTodoAtom = atom(
   () => "",
   (get, set) => {
     set(todo, handleAddTodo(get(todo), get(newTodo)));
+    set(newTodo, { title: "" });
   }
 );
 
 const deleteTodoAtom = atom(
-    () => "",
-    (get, set, id) => {
-        set(todo, handleRemoveTodo(get(todo), id));
-    }
-)
+  () => "",
+  (get, set, id) => {
+    set(todo, handleRemoveTodo(get(todo), id));
+  }
+);
 
-export { todo, newTodo, addTodoAtom, deleteTodoAtom };
+const completeTodoAtom = atom(
+  () => "",
+  (get, set, id) => {
+    set(todo, handleCompleteTodo(get(todo), id));
+  }
+);
+
+const unCompleteAtom = atom(
+  () => "",
+  (get, set, id) => {
+    set(todo, handleUncompleteTodo(get(todo), id));
+  }
+);
+
+export {
+  todo,
+  newTodo,
+  addTodoAtom,
+  deleteTodoAtom,
+  completeTodoAtom,
+  unCompleteAtom,
+};
